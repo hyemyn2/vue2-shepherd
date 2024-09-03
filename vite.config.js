300c7ed7c0876f46e3e5a25348c083c2f4e734fa
hyemyn2 <hyemyn2@gmail.com>
2024-09-03 16:06:28 +0900

    feat: build css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import { resolve } from 'path'
-
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/assets/shepherd.css',
+                    dest: '.'
+                }
+        ]
+        })
+    ],
     publicDir: false,
     build: {
         type: ['es', 'umd'],
